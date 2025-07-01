c8e4dcd4aeb18459dec2e853efaaa4d45e4c04b9
yangjunsik <wnstlr1379@naver.com>
2025-07-01 16:46:27 +0900

    swagger 연동

--- src/main/java/com/clip/config/OpenAPIConfig.java
+++ src/main/java/com/clip/config/OpenAPIConfig.java
@@ -1,2 +1,16 @@
-package com.clip.config;public class OpenAPIConfig {
+package com.clip.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Clip API",
+                description = "자동 요약 클립 공유 플랫폼",
+                version = "v1.0"
+        )
+)
+@Configuration
+public class OpenAPIConfig {
 }
