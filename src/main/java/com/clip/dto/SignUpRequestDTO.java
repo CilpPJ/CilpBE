0398f1d6b2c56df3ffefbb97828d67ece0d02f89
yangjunsik <wnstlr1379@naver.com>
2025-07-04 17:13:55 +0900

    fix: 쿠키 개발환경에서 http도 허용되게 수정 (Refs #2)

--- src/main/java/com/clip/dto/SignUpRequestDTO.java
+++ src/main/java/com/clip/dto/SignUpRequestDTO.java
@@ -8,7 +8,10 @@ import lombok.Setter;
 @Setter
 @Schema(description = "회원가입 요청 DTO")
 public class SignUpRequestDTO {
+    @Schema(description = "아이디", example = "testuser")
     private String userId;
+    @Schema(description = "비밀번호", example = "testpass")
     private String password;
+    @Schema(description = "닉네임", example = "tester")
     private String nickName;
 }
